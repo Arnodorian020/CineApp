a6326b4ca55c127b306446eea851c3d22eeb401d
DogeFox071101 <lgfalconch@hotmail.com>
2022-05-18 13:12:31 -0500

    Pantalla 5 lista
    Carga de pantalla Sobre Nosotros

--- app/src/main/java/pe/edu/ulima/pm/cineapp/fragments/NosotrosFragment.kt
+++ app/src/main/java/pe/edu/ulima/pm/cineapp/fragments/NosotrosFragment.kt
@@ -4,15 +4,29 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 import pe.edu.ulima.pm.cineapp.R
 
 class NosotrosFragment : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        activity?.title = "Sobre Nosotros"
+    }
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_nosotros, container, false)
+        val v = inflater.inflate(R.layout.fragment_nosotros, container, false)
+        val btn = v.findViewById<Button>(R.id.btnBack)
+        btn?.setOnClickListener {
+            println("a")
+            val ft = requireActivity().supportFragmentManager
+            ft.popBackStack()
+            //ft.replace(R.id.fcvEleccion, CarteleraFragment())
+        }
+        return v
     }
 }
