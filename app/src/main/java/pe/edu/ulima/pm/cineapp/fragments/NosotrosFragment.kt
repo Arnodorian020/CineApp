64ee6e17ee72511592c4d3261fe9d3b3437d160f
bachmaster <brunoandrechirinosmolina@gmail.com>
2022-05-18 17:32:51 -0500

    Formato  y tipografía del fragment_nosotros y algunos strings agregado al strings.xml Se agrego un bg para el fragment_nosotros.xml

--- app/src/main/java/pe/edu/ulima/pm/cineapp/fragments/NosotrosFragment.kt
+++ app/src/main/java/pe/edu/ulima/pm/cineapp/fragments/NosotrosFragment.kt
@@ -7,9 +7,12 @@ import android.view.ViewGroup
 import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
+import com.google.android.material.navigation.NavigationView
 import pe.edu.ulima.pm.cineapp.R
 
 class NosotrosFragment : Fragment() {
+    private lateinit var mNviListaPeliculas : NavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         activity?.title = "Sobre Nosotros"
@@ -22,9 +25,9 @@ class NosotrosFragment : Fragment() {
         val v = inflater.inflate(R.layout.fragment_nosotros, container, false)
         val btn = v.findViewById<Button>(R.id.btnBack)
         btn?.setOnClickListener {
-            println("a")
             val ft = requireActivity().supportFragmentManager
             ft.popBackStack()
+
             //ft.replace(R.id.fcvEleccion, CarteleraFragment())
         }
         return v
